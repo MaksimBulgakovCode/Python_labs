74df94adace779932001d251c78083bec4fd2543
MaksimBulgakovCode <maximpisarew2007@gmail.com>
2026-01-21 17:48:47 +1000

    Add files via upload

---
+++ kt1.py
@@ -0,0 +1,19 @@
+import random
+
+def start():
+    count = 0
+    while True:
+        say = input("Вы: ")
+        if say == "ПОКА!":
+            count += 1
+        else:
+            count = 0
+        if count == 3:
+            print("Бабушка: ДО СВИДАНИЯ, ДОРОГОЙ!")
+            break
+        elif say.isupper():
+            print(f"Бабушка: НЕТ, НИ РАЗУ С {random.randint(1930, 1980)} ГОДА!")
+        else:
+            print("Бабушка: АСЬ?! ГОВОРИ ГРОМЧЕ, ВНУЧЕК")
+
+start()
